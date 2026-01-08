d81f616f57d87305c3264d84af6b6a1bde8d694c
Sojo2115 <patryk79745@gmail.com>
2026-01-08 14:37:00 +0100

    implementacja trwalosci danych - zapis i odczyt z pliku

--- funkcje.c
+++ funkcje.c
@@ -6,7 +6,9 @@ void wyswietlMenu() {
     printf("\n--- MENU GLOWNE ---\n");
     printf("1. Dodaj nowy wpis\n");
     printf("2. Wyswietl baze danych\n");
-    printf("3. Zakoncz program\n");
+    printf("3. Zapisz do pliku\n");
+    printf("4. Wczytaj z pliku\n");
+    printf("5. Zakoncz program\n");
     printf("Twoj wybor: ");
 }
 
@@ -41,4 +43,40 @@ void wyswietlWszystkie(const Rekord *baza, int licznik) {
     for (int i = 0; i < licznik; i++) {
         printf("%d | %s | %.2f\n", baza[i].id, baza[i].nazwa, baza[i].wartosc);
     }
+}
+
+void zapiszDoPliku(const Rekord *baza, int licznik) {
+    FILE *plik = fopen(PLIK_DANE, "w");
+    if (plik == NULL) {
+        printf("[BLAD] Nie mozna otworzyc pliku do zapisu!\n");
+        return;
+    }
+
+    // Zapisujemy ilosc rekordow jako pierwsza liczbe
+    fprintf(plik, "%d\n", licznik);
+
+    for (int i = 0; i < licznik; i++) {
+        fprintf(plik, "%d %s %.2f\n", baza[i].id, baza[i].nazwa, baza[i].wartosc);
+    }
+
+    fclose(plik);
+    printf("[SUKCES] Dane zapisane do pliku %s.\n", PLIK_DANE);
+}
+
+void wczytajZPliku(Rekord *baza, int *licznik) {
+    FILE *plik = fopen(PLIK_DANE, "r");
+    if (plik == NULL) {
+        printf("[INFO] Brak pliku z danymi lub blad odczytu.\n");
+        return;
+    }
+
+    // Wczytujemy liczbe rekordow
+    fscanf(plik, "%d", licznik);
+
+    for (int i = 0; i < *licznik; i++) {
+        fscanf(plik, "%d %s %f", &baza[i].id, baza[i].nazwa, &baza[i].wartosc);
+    }
+
+    fclose(plik);
+    printf("[SUKCES] Wczytano %d rekordow z pliku.\n", *licznik);
 }
